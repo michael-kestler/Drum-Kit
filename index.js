f952534933265a1d8681533aa29f7e63fb0817ee
michael-kestler <michael.s.kestler@gmail.com>
2022-10-19 22:00:36 -0700

    used 'this' so that text on each button turns white when clicked

--- index.js
+++ index.js
@@ -5,10 +5,12 @@ for (var i = 0; i<document.querySelectorAll(".drum").length; i++){
 
 
   document.querySelectorAll(".drum") [i].addEventListener("click", function () {
-    var audio = new Audio('sounds/tom-1.mp3');
-    audio.play();
 
+    this.style.color = "white";
 
   });
+
+  // var audio = new Audio('sounds/tom-1.mp3');
+  // audio.play();
 
 }
