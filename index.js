245c9c2aa34b1bb28abda5e20ce2daf5f1c37fb8
michael-kestler <michael.s.kestler@gmail.com>
2022-10-19 21:18:15 -0700

    Added function to make drum sound when drum is clicked

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
+var audio = new Audio('sounds/tom-1.mp3');
+audio.play();
+
+
 for (var i = 0; i<document.querySelectorAll(".drum").length; i++){
+
+
   document.querySelectorAll(".drum") [i].addEventListener("click", function () {
-    alert("I got clicked");
+    audio.play();
+
+
   });
 
 }
